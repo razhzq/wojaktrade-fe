d9548deed05313624df7fe8eab5edf3d4dc71815
michaeldepp <michaeldepp7@gmail.com>
2024-11-03 22:30:32 +0800

    Fix : styling color

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,9 @@ import Calculator from './components/Calculator';
 
 export default function Home() {
 	return (
-		<div className="bg-black">
+		<div className="bg-black min-h-screen">
 			<Header />
-			<div className="flex justify-center w-4/5 sm:w-3/5 max-w-screen-lg mx-auto pt-8">
+			<div className="flex justify-center w-4/5 sm:w-3/5 max-w-screen-lg mx-auto pt-16">
 				<Calculator />
 			</div>
 			<Footer />
